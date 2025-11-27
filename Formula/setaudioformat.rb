ad2f1a3888455b3db35af19cbb609a1e68339235
afawe3331 <bichpul@naver.com>
2025-11-27 14:37:16 +0900

    Change build method to use Makefile instead of Xcode

--- Formula/setaudioformat.rb
+++ Formula/setaudioformat.rb
@@ -5,21 +5,16 @@ class Setaudioformat < Formula
   version "latest"
   license "MIT"
 
-  depends_on xcode: :build
+  # Xcode 전체 대신 Command Line Tools로도 빌드 가능
+  depends_on "make" => :build
 
   def install
-    # Xcode 프로젝트 빌드
-    system "xcodebuild", "-project", "setaudioformat.xcodeproj",
-                         "-scheme", "setaudioformat",
-                         "-configuration", "Release",
-                         "SYMROOT=build"
-
-    # 빌드된 실행 파일 설치
-    bin.install "build/Release/setaudioformat"
+    # Makefile 기반 빌드
+    system "make"
+    bin.install "setaudioformat"
   end
 
   test do
-    # 설치 확인: 도움말 출력이 되는지 체크
     assert_match "Usage", shell_output("#{bin}/setaudioformat --help", 1)
   end
 end
